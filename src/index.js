828393a497a30d6ce6bdb05448251066c77998a3
Kowalskiexe <kowalski.exe@gmail.com>
2023-02-09 20:26:48 +0100

    basic build done

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@
 
 import path from 'node:path';
 import { open, opendir, mkdir } from 'node:fs/promises';
-import { Buffer } from 'node:buffer';
 import { htmlTags } from './tags.js';
 
 
-let inDir = 'unset';
-let outDir = 'unset';
-process.argv.forEach((arg) => {
-    if (arg.startsWith('--in='))
-        inDir = arg.slice('--in='.length);
-    if (arg.startsWith('--out='))
-        outDir = arg.slice('--out='.length);
-});
-if (outDir === 'unset')
-    outDir = path.join(inDir, '../build');
-if (inDir === 'unset') {
-    console.error('input directory not spicified, use --in=dir_path');
-    process.exit(1);
-} else {
-    console.log(`input dir: ${inDir}`);
-    console.log(`output dir: ${outDir}`);
+function parseArgs() {
+    let inDir = 'unset';
+    let outDir = 'unset';
+    process.argv.forEach((arg) => {
+        if (arg.startsWith('--in='))
+            inDir = arg.slice('--in='.length);
+        if (arg.startsWith('--out='))
+            outDir = arg.slice('--out='.length);
+    });
+    if (outDir === 'unset')
+        outDir = path.join(inDir, '../build');
+    if (inDir === 'unset') {
+        console.error('Error: input directory not spicified, use --in=dir_path');
+        process.exit(1);
+    } else {
+        console.log(`input dir: ${inDir}`);
+        console.log(`output dir: ${outDir}`);
+    }
+    return {inDir: inDir, outDir: outDir};
 }
 
 async function parseDir(dirPath, inDir, digraph, tagFileMap) {
@@ -57,7 +59,7 @@ function getCustomTags(html) {
     if (!tags)
         return new customTags;
     tags.forEach(tag => {
-        if (htmlTags.every(v => getTagName(tag) !== getTagName(v)))
+        if (isCustomTag(tag))
             customTags.add(getTagName(tag));
     });
     return customTags;
@@ -69,17 +71,8 @@ function isCustomTag(tag) {
 
 async function parseFile(filePath, inDir, digraph, tagFileMap) {
     const file = await open(filePath);
-    const customTags = new Set()
-    for await (const line of file.readLines()) {
-        const tagRegex = /<.*>/g;
-        const tags = line.match(tagRegex);
-        if (!tags)
-            continue;
-        for (const tag of tags) {
-            if (isCustomTag(tag))
-                customTags.add(getTagName(tag));
-        }
-    }
+    const content = await file.readFile({encoding: 'UTF-8'});
+    const customTags = getCustomTags(content);
     // remove .html from file name
     const tag = path.basename(filePath).slice(0, -5);
     digraph.set(tag, customTags);
@@ -108,58 +101,59 @@ function visit(vertex, digraph, states, output) {
         if (states.get(next) === 'processed')
             throw new Error('The graph isn\'t acyclic!');
         if (states.get(next) === 'unvisited')
-            visit(next, digraph, states, output);
-    }
+            visit(next, digraph, states, output); }
     states.set(vertex, 'visited');
     output.push(vertex);
 }
 
-const digraph = new Map();
-const tagFileMap = new Map();
-await parseDir(inDir, inDir, digraph, tagFileMap);
-
-console.log('digraph');
-console.log(digraph);
-
-console.log('tagFileMap');
-console.log(tagFileMap);
-
-console.log('sorted');
-const sorted = sortTopologically(digraph);
-console.log(sorted);
-
-sorted.reverse();
-for (const tag of sorted) {
-    await buildTag(tag);
+async function build(inDir, outDir) {
+    const digraph = new Map();
+    const tagFileMap = new Map();
+    await parseDir(inDir, inDir, digraph, tagFileMap);
+    
+    const sorted = sortTopologically(digraph);
+    
+    sorted.reverse();
+    for (const tag of sorted) {
+        await buildTag(tag, tagFileMap, inDir, outDir);
+    }
 }
-async function buildTag(tagName) {
-    const inPath = path.join(inDir, tagFileMap.get(tagName)) + '.html';
-    const outPath = path.join(outDir, tagFileMap.get(tagName)) + '.html';
-    console.log(`build ${tagName} at ${inPath}, into ${outPath}`);
+
+function getInPath(inDir, relativePath) {
+    return path.join(inDir, relativePath + '.html');
+}
+
+function getOutPath(outDir, relativePath) {
+    return path.join(outDir, relativePath + '.html');
+}
+
+async function buildTag(tagName, tagFileMap, inDir, outDir) {
+    const inPath = getInPath(inDir, tagFileMap.get(tagName));
+    const outPath = getOutPath(outDir, tagFileMap.get(tagName));
     const inFile = await open(inPath);
     await mkdir(path.dirname(outPath), { recursive: true });
     const outFile = await open(outPath, 'w');
-    for await (const line of inFile.readLines()) {
-        // TODO: total remake
-        const tagRegex = /<.*>/g;
-        const tags = line.match(tagRegex);
-        if (tags) {
-            for (const tag of tags) {
-                if (!isCustomTag(tag))
-                    continue;
-                // paste custom tag
-                console.log(`pasting ${tag}`);
-                const customPath = path.join(inDir, tagFileMap.get(getTagName(tag))) + '.html';
-                const customFile = await open(customPath);
-                const content = await customFile.readFile();
-                console.log(content);
-                await outFile.write(content);
-                await customFile.close();
-            }
+    let content = await inFile.readFile({encoding: 'UTF-8'});
+    for (const [tag, path] of tagFileMap) {
+        if (!content.includes(`<${tag}>`))
+            continue;
+        if (tag === tagName) {
+            // prevent recursion
+            console.error('Error: recursion is not allowed');
+            continue;
         }
-        // TODO: dont just paste
-        await outFile.write(line + '\n');
+        const fullPath = getInPath(inDir, path);
+        const foreignFile = await open(fullPath);
+        const foreignContent = await foreignFile.readFile({encoding: 'UTF-8'});
+        content = content.replaceAll(`<${tag}>`, foreignContent);
     }
+    outFile.writeFile(content, {encoding: 'UTF-8'});
     await outFile.close();
 }
 
+async function main() {
+    const {inDir, outDir} = parseArgs();
+    await build(inDir, outDir);
+    console.log('build done');
+}
+main();
