4579fe2570464e73e75b5fe89850fb94d35d5d27
Kowalskiexe <kowalski.exe@gmail.com>
2023-02-10 15:21:56 +0100

    added bin to package.json

--- src/index.js
+++ src/index.js
@@ -12,14 +12,22 @@ function parseArgs() {
         if (arg.startsWith('--out='))
             outDir = arg.slice('--out='.length);
     });
+    // remove './' from path
+    if (inDir.startsWith('./'))
+        inDir = inDir.slice(2)
+    if (outDir.startsWith('./'))
+        outDir = outDir.slice(2)
+    // remove trailing '/' from path
+    if (inDir.endsWith('/'))
+        inDir = inDir.slice(0, -1)
+    if (outDir.endsWith('/'))
+        outDir = outDir.slice(0, -1)
+
     if (outDir === 'unset')
         outDir = path.join(inDir, '../build');
     if (inDir === 'unset') {
         console.error('Error: input directory not spicified, use --in=dir_path');
         process.exit(1);
-    } else {
-        console.log(`input dir: ${inDir}`);
-        console.log(`output dir: ${outDir}`);
     }
     return {inDir: inDir, outDir: outDir};
 }
@@ -28,6 +36,8 @@ function parseArgs() {
 
 async function main() {
     const {inDir, outDir} = parseArgs();
+    console.log(`input dir: ${inDir}`);
+    console.log(`output dir: ${outDir}`);
     await build(inDir, outDir);
     console.log('build done');
 }
